3da4b6c2d1212e0b7ace3bb63b24326d23be754d
KieranB97x <144362451+KieranB97x@users.noreply.github.com>
2024-03-15 09:46:05 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Components/WeatherApp/WeatherApp";
+import App from "./App";
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
